61e2084f9abd3db309151bb41655c2ac59d29736
Willy Tarreau <w@1wt.eu>
2019-01-16 11:42:58 +0100

    Add a set of scripts to start the services and sidecars
    These ones simply start haproxy and the application with all the
    URIs and ports correctly set.

---
+++ start-mytime.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+export PORT=2004
+export SOURCE=svc-mytime.py
+export SERVICE=127.0.0.2
+export SIDECAR=127.0.0.3
+export TIME_URL="http://$SIDECAR:2$PORT/GetTime"
+export USER_URL="http://$SIDECAR:2$PORT/UserAttr"
+export LOG_URL="http://$SIDECAR:2$PORT/Log"
+export HAPROXY=${HAPROXY:-haproxy}
+export PYTHON=${PYTHON:-python}
+export PATH=".:$PATH"
+
+if [ -s "/tmp/haproxy-$PORT.pid" ]; then
+        pid=$(cat /tmp/haproxy-$PORT.pid)
+        kill $pid >/dev/null 2>&1
+fi
+
+if [ -z "$1" -o "$1" = "start" ]; then
+        "$HAPROXY" -D -f sidecar.cfg -p "/tmp/haproxy-$PORT.pid"
+        "$PYTHON" "$SOURCE" "$SERVICE:1$PORT" "$TIME_URL" "$USER_URL" "$LOG_URL"
+fi
